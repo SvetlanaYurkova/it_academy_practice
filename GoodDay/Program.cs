f95dfa49c1545b6c62845325f0657d1ca2f95364
Юркова Светлана <svyurkova@gmail.com>
2020-11-11 21:11:32 +0300

    Первый вариант

---
+++ GoodDay/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoodDay
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Greeting greeting = new Greeting();
+      greeting.print();
+    }
+  }
+}
