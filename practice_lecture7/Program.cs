f98021a34ff46f0d78d3d480322303b340819cb9
Svetlana Yurkova <svyurkova@gmail.com>
2020-11-16 20:59:06 +0300

    task2

--- practice_lecture7/Program.cs
+++ practice_lecture7/Program.cs
@@ -4,13 +4,33 @@ namespace practice_lecture7
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             int[] array = CreateArray();
+            ChangeArray();
+ 
         }
 
-        static int[] CreateArray() {
+        private static int[] CreateArray() {
             return new int[5];
         }
+
+        static void ChangeArray()
+        {
+            Object[] ArrayOfObjects = new Object[] { 23, 'A', "Hello" };
+            for (int i = 0; i < ArrayOfObjects.Length; i++)
+            {
+                Console.WriteLine(ArrayOfObjects[i]);
+            }
+
+            ArrayOfObjects[0] = (int)ArrayOfObjects[0] + 10;
+            ArrayOfObjects[2] = ArrayOfObjects[2]  + ", guys!";
+
+            for (int i = 0; i < ArrayOfObjects.Length; i++)
+            {
+                Console.WriteLine(ArrayOfObjects[i]);
+            }
+        }
+
     }
 }
