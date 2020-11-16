e6dd1ed5fb0e85afabb129369b4638c3712a93c1
Svetlana Yurkova <svyurkova@gmail.com>
2020-11-16 20:32:31 +0300

    task1

---
+++ practice_lecture7/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace practice_lecture7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = CreateArray();
+        }
+
+        static int[] CreateArray() {
+            return new int[5];
+        }
+    }
+}
