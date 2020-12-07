c6874752c4fd7f997797b7cf19620e70c5c6341a
Svetlana Yurkova <svyurkova@gmail.com>
2020-12-07 19:45:09 +0300

    lecture12

---
+++ practice_lecture12/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace practice_lecture12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape[] shapes = {new Square(2), new Circle(3), new Triangle(5), new Square(4), new Circle(2)};
+            for (int i = 0; i < shapes.Length; i++)
+            {
+                shapes[i].GetInfo();
+            }
+
+        }
+    }
+}
