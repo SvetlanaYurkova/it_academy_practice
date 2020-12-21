f7e4f4a448bc85c3e71b0eff3a938336a9dd0cd0
Svetlana Yurkova <svyurkova@gmail.com>
2020-12-21 19:15:46 +0300

    commit1

---
+++ task_1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UniqueItem ui1 = new UniqueItem();
+            UniqueItem ui2 = new UniqueItem(10);
+            UniqueItem ui3 = new UniqueItem();
+
+        }
+    }
+}
